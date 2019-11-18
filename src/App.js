154edd3e1d2e1fb504ae5eec111544cc113f9e1d
Gerrit Alex <galex@mpunkt.com>
2019-11-18 15:38:21 +0100

    add basic component

--- src/App.js
+++ src/App.js
@@ -9,46 +9,52 @@ import { HomeRoute, LoginRoute } from './routes/public';
 import { ProfileRoute, DashboardRoute } from './routes/private';
 import RedirectToHome from './routes/RedirectToHome';
 import { AuthContext } from './context/AuthContext';
+import { Footer } from './components';
 
 export default function App() {
   const { isLoggedIn, handleLogout } = useContext(AuthContext);
 
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to='/'>Home</NavLink>
-          </li>
-
-          {isLoggedIn ? (
-            <>
-              <li>
-                <NavLink to='/profile'>Profile</NavLink>
-              </li>
-              <li>
-                <button onClick={handleLogout} type='button'>
-                  Logout
-                </button>
-              </li>
-            </>
-          ) : (
+      <header>
+        <nav>
+          <ul>
             <li>
-              <NavLink to='/login'>Login</NavLink>
+              <NavLink to='/'>{isLoggedIn ? 'Dashboard' : 'Home'}</NavLink>
             </li>
-          )}
-        </ul>
-      </nav>
-      <Switch>
-        <Route
-          path='/'
-          exact
-          component={isLoggedIn ? DashboardRoute : HomeRoute}
-        />
-        <Route path='/login' exact component={LoginRoute} />
-        <Route path='/profile' exact component={ProfileRoute} />
-        <Route component={RedirectToHome} />
-      </Switch>
+
+            {isLoggedIn ? (
+              <>
+                <li>
+                  <NavLink to='/profile'>Profile</NavLink>
+                </li>
+                <li>
+                  <button onClick={handleLogout} type='button'>
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <li>
+                <NavLink to='/login'>Login</NavLink>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Switch>
+          <Route
+            path='/'
+            exact
+            component={isLoggedIn ? DashboardRoute : HomeRoute}
+          />
+          <Route path='/login' exact component={LoginRoute} />
+          <Route path='/profile' exact component={ProfileRoute} />
+          <Route component={RedirectToHome} />
+        </Switch>
+      </main>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Footer() {
+  return <footer>(c) {new Date().getFullYear()}</footer>;
+}
