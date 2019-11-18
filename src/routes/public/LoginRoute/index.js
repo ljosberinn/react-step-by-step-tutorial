4c054c6348820d12c9ec07118aa84cde9705c242
Gerrit Alex <galex@mpunkt.com>
2019-11-18 12:54:50 +0100

    add login form & handleChange

--- src/routes/public/LoginRoute/index.js
+++ src/routes/public/LoginRoute/index.js
@@ -2,12 +2,77 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 export default function LoginRoute() {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function handleChange({ target }) {
+    const { name, value, type, checked } = target;
+
+    console.log(name, value, type, checked);
+  }
+
   return (
     <>
       <Helmet>
         <title>Login</title>
       </Helmet>
-      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit} spellCheck={false} autoComplete='off'>
+        <fieldset>
+          <legend>
+            <h1>Login</h1>
+          </legend>
+
+          <label>
+            Username
+            <input
+              type='text'
+              placeholder='username'
+              required
+              name='username'
+              onChange={handleChange}
+            />
+          </label>
+
+          <br />
+
+          <label>
+            Password
+            <input
+              type='password'
+              placeholder='password'
+              required
+              name='password'
+              onChange={handleChange}
+            />
+          </label>
+
+          <br />
+
+          <label>
+            Repeat password
+            <input
+              type='password'
+              placeholder='repeat password'
+              required
+              name='repeatPassword'
+              onChange={handleChange}
+            />
+          </label>
+
+          <br />
+
+          <label>
+            Remember me
+            <input type='checkbox' name='rememberMe' onChange={handleChange} />
+          </label>
+
+          <br />
+
+          <button type='submit'>Login</button>
+        </fieldset>
+      </form>
     </>
   );
 }
